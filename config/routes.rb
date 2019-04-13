fa31b574a57c1a414090eed636b0ea8ddf356bc8
JerYOMat <jeremy.emata@gmail.com>
2019-04-13 14:45:05 -0400

    fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resource :companies, only: [:index]
       resource :users, only: [:create, :update, :destroy]
       resource :favorites, only: [:create, :destroy]
-      get '/companies/:ticker'. to: 'companies/fins_by_ticker'
+      get '/companies/:ticker', to: 'companies/fins_by_ticker'
       post '/login', to: 'users#login'
       post '/logout', to: 'users#logout'
     end 
